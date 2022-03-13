cac3fb7d607e982fb1447f126d14abcc964d2ab5
Colin Blokkeel <cblokkeeltech@gmail.com>
2022-03-13 12:59:41 +0100

    POST /launches front end

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -2,20 +2,32 @@ const API_URL = 'http://localhost:8000'
 
 // Load planets and return as JSON.
 async function httpGetPlanets() {
-    const response = await fetch(`${API_URL}/planets`);
-    return await response.json();
+    const res = await fetch(`${API_URL}/planets`);
+    return await res.json();
 }
 
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  const response = await fetch(`${API_URL}/launches`);
-  const data = await response.json();
+  const res = await fetch(`${API_URL}/launches`);
+  const data = await res.json();
   return data.sort((a, b) => a.flightNumber - b.flightNumber)
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(launch)
+    })
+  } catch(err) {
+    return {
+      ok: false
+    }
+  }
 }
 
 async function httpAbortLaunch(id) {
