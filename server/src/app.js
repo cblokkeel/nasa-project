b5e811dae218593925a89ccf32711c8c5a9bc01d
Colin Blokkeel <cblokkeeltech@gmail.com>
2022-03-13 11:08:12 +0100

    npm deploy command

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const planetsRouter = require('./routes/planets/planets.router')
+const path = require('path')
 
 const app = express()
 
@@ -8,7 +9,11 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }))
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter)
 
+app.get('/', (req, res) => {
+    res.send(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app
